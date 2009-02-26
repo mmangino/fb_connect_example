6fe8f8d5863b15610a42052734c676fd8230eb32
Michael Mangino <mmangino@elevatedrails.com>
2009-02-26 12:49:49 -0500

    Working signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.create!(params[:user])
+    redirect_to @user
+  rescue ActiveRecord::RecordInvalid => e
+    @user = e.record
+    render :action=>"new"
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 end
